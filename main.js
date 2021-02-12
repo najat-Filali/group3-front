8f4d0b14fcafc108e286986859967ccc1d10a138
anaelleray <anaelleray7@gmail.com>
2021-02-12 15:27:30 +0100

    get api

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("console.log -> server running from main.js");
 
 const getApi = () => {
 
-    let result = fetch('http://localhost:3000/')
+    let result = fetch('http://grp3.360medics.com:8080/')
     console.log("call ? ->", result);
 }
 getApi();
