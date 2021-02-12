d005ab5ac6b5d244647e24d65a1f3577fa6e0076
anaelleray <anaelleray7@gmail.com>
2021-02-12 14:51:19 +0100

    add fetch url to back project

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+
 console.log("console.log -> server running from main.js");
+
+const getApi = () => {
+
+    let result = fetch('http://localhost:3000/')
+    console.log("call ? ->", result);
+}
+getApi();
